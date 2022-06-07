d07da79efdce284dd2c71565e367bf3cb21b23c4
sbin0819 <sbinha123@gmail.com>
2022-06-08 08:48:02 +0900

    set: basic setup

---
+++ src/components/ui/tab/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tab = () => {
+  return <div className="text-orange-400">tab</div>;
+};
+
+export default Tab;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages/'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components/'),
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, 'src/hooks/'),
+      },
+      {
+        find: '@ui',
+        replacement: path.resolve(__dirname, 'src/components/ui'),
+      },
+    ],
+  },
+});
